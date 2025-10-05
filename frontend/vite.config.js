d7bda5f87a54a887c3bab33160d6271e84b345ff
Tam Tran <itistamtran@gmail.com>
2025-10-05 00:14:39 -0700

    Fix Vite config for production and cleanup .gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    proxy: {
-      '/predict': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  preview: {
-    proxy: {
-      '/predict': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+  base: "/",
+});
